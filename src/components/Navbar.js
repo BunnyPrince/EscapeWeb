1d494adfefa5667a0d156aef180e900acbdf0d66
BunnyPrince <71982648+BunnyPrince@users.noreply.github.com>
2023-05-07 16:40:25 -0400

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar(){
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <h1>Hello World</h1>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
